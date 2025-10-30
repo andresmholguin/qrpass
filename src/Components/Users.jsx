474e45aa19f1dbd920bd066b5f3fd4a522ccd720
Andres Holguin <andresmholguin@gmail.com>
2025-10-30 01:03:59 -0500

    Agregar logo y título en el componente Users; mejorar diseño y presentación

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,3 +1,12 @@
+import valleinLogo from "../assets/valleinlogo.png";
+
 export const Users = () => {
-  return <div>Users</div>;
+  return (
+    <div className="w-[350px] mx-auto flex flex-col">
+      <picture className="relative mx-auto mb-4">
+        <img src={valleinLogo} alt="Logo Valle In" />
+      </picture>
+      <h1 className="text-4xl text-center mb-8">Usuarios</h1>
+    </div>
+  );
 };
