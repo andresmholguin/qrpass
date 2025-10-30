6a8864724384817f467ee5bd5fb650e17a33d9a6
Andres Holguin <andresmholguin@gmail.com>
2025-10-29 21:57:12 -0500

    Agregar cliente Supabase y actualizar formulario de CheckIn; mejorar estilos y campos

--- src/Components/CheckIn.jsx
+++ src/Components/CheckIn.jsx
@@ -1,49 +1,61 @@
+import valleinLogo from "../assets/valleinlogo.png";
 export const CheckIn = () => {
   return (
     <div className="w-[350px] mx-auto flex flex-col">
-      <h1 className="text-3xl mb-8">Registro de Asistente</h1>
+      <picture className=" relative mx-auto mb-4">
+        <img src={valleinLogo} alt="" />
+      </picture>
+      <h1 className="text-4xl text-center mb-8">Registro de Asistente</h1>
       <p className="font-light">Completa los datos para el registro:</p>
       <form className="flex flex-col gap-4 mt-4">
-        <div className="flex flex-col">
-          <label className="font-thin" htmlFor="nombre">
+        <div className="flex flex-col gap-1">
+          <label className="font-thin text-[1.3rem]" htmlFor="ni">
+            Documento identidad:
+          </label>
+          <input
+            className="bg-gray-100 text-Secondary p-2 rounded-md "
+            type="number"
+            id="ni"
+            inputMode="numeric"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label className="font-thin text-[1.3rem]" htmlFor="nombre">
             Nombre completo:
           </label>
           <input
-            className="bg-gray-300 text-Secondary p-2 rounded-md "
+            className="bg-gray-100 text-Secondary p-2 rounded-md uppercase "
             type="text"
             id="nombre"
           />
         </div>
-        <div className="flex flex-col">
-          <label className="font-thin" htmlFor="ni">
-            Documento identidad:
-          </label>
-          <input
-            className="bg-gray-300 text-Secondary p-2 rounded-md "
-            type="text"
-            id="ni"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label className="font-thin" htmlFor="ni">
+        <div className="flex flex-col gap-1">
+          <label className="font-thin text-[1.3rem]" htmlFor="phone">
             Celular:
           </label>
           <div className="flex gap-2">
             <input
-              className="bg-gray-300 text-Secondary w-15 p-2 rounded-md text-center "
+              className="bg-gray-100 text-Secondary w-15 p-2 rounded-md text-center "
               type="numbre"
-              id="ni"
+              id="prephone"
+              inputMode="numeric"
+              pattern="^\+[0-9]*"
+              maxLength="3"
+              placeholder="+57"
             />
             <input
-              className="bg-gray-300 text-Secondary p-2 w-full rounded-md "
+              className="bg-gray-100 text-Secondary p-2 w-full rounded-md "
               type="numbre"
-              id="ni"
+              id="phone"
+              inputMode="tel"
+              pattern="[0-9]*"
+              maxLength="10"
             />
           </div>
         </div>
         <button
           type="submit"
-          className="bg-Primary text-Secondary px-4 py-2 rounded-md mt-8 hover:bg-gray-900 hover:text-Primary transition-colors cursor-pointer font-semibold "
+          className="bg-Primary text-Secondary px-4 py-2 rounded-md mt-8 hover:bg-gray-900 hover:text-Primary transition-colors cursor-pointer font-semibold text-lg"
         >
           Registrar
         </button>
