325f71c56f5f0eef26fee22bdf9c9ce31a88c232
Andres Holguin <andresmholguin@gmail.com>
2025-11-03 12:44:40 -0500

    Refactor rutas y lógica de ProtectedRoute para usar Zustand y simplificar la verificación de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import {
-  Dashboard,
   Users,
   Login,
   CheckIn,
@@ -9,23 +8,24 @@ import {
   CreateUsers,
 } from "./Components/index";
 import { ProtectedRoute } from "./Components/utils/ProtectedRoute";
+import { useUserStore } from "./Components/store/userStore";
 
 function App() {
-  // const [user, setUser] = useState(null);
+  const user = useUserStore((state) => state.user); // ✅ leer directo de Zustand
 
   return (
     <div className="w-[370px] sm:container flex align-center flex-col lg:px-4 h-full">
       <Header />
       <Routes>
+        {/* Public routes */}
         <Route index element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/registers" element={<Users />} />
-          <Route path="/checkin" element={<CheckIn />} />
-          <Route path="/reports" element={<Reports />} />
+
+        <Route element={<ProtectedRoute isAllowed={!!user} />}>
           <Route path="/createuser" element={<CreateUsers />} />
-          <Route path="/*" element={<Reports />} />
+          <Route path="/checkin" element={<CheckIn />} />
+          <Route path="/registers" element={<Users />} />
+          <Route path="/reports" element={<Reports />} />
         </Route>
       </Routes>
     </div>
--- src/Components/utils/ProtectedRoute.jsx
+++ src/Components/utils/ProtectedRoute.jsx
@@ -1,15 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useUserStore } from "../store/userStore";
 
-export const ProtectedRoute = ({ redirectTo = "/login" }) => {
-  const storedUser = useUserStore((state) => state.user);
-
-  if (!storedUser) {
+export function ProtectedRoute({ isAllowed, redirectTo = "/login" }) {
+  if (!isAllowed) {
     return <Navigate to={redirectTo} replace />;
   }
-  if (storedUser.user_role == 3245) {
-    console.log("No tiene rol");
-    return <Navigate to={"/checkin"} replace />;
-  }
+
   return <Outlet />;
-};
+}
