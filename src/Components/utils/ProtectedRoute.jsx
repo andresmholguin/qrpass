4fceda7a3d43864a24bea6c165ffecff1658a9dd
Andres Holguin <andresmholguin@gmail.com>
2025-11-02 21:55:30 -0500

    Actualizar logo y simplificar lógica en ProtectedRoute eliminando comentarios innecesarios.

--- src/Components/utils/ProtectedRoute.jsx
+++ src/Components/utils/ProtectedRoute.jsx
@@ -9,13 +9,9 @@ export const ProtectedRoute = ({ redirectTo = "/login" }) => {
     return <Navigate to={redirectTo} replace />;
   }
 
-  // if (storedUser) {
-  // console.log("localStorage tiene datos");
-  // const parsedUser = JSON.parse(storedUser);
   // if (parsedUser.user_role !== 2525) {
   //   console.log("No tiene rol");
   //   return <Navigate to={redirectPath} replace />;
-  // }
   // }
   return <Outlet />;
 };
